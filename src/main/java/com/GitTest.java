5c21ec4665a198da11f5c3c151601b361a0a93e8
santuheV <90965069+santuheV@users.noreply.github.com>
2021-09-21 18:40:00 +0800

    Update GitTest.java

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -3,5 +3,6 @@ package com;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("push--->Github");
+        System.out.println("pull--->Github");
     }
 }
